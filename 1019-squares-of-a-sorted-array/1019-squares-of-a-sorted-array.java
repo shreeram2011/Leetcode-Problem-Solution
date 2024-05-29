6b78dc0723d2f323ea0dcb9286ce2d4180d7d6e8
Shreeram Yadav <113875685+shreeram2011@users.noreply.github.com>
2024-05-29 15:41:55 +0530

    Update 1019-squares-of-a-sorted-array.java

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,3 +1,20 @@
+//First Solution
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int result [] = new int[n];
+        
+        for(int i = 0; i < n; i ++)
+            result[i] = nums[i] * nums[i];
+        Arrays.sort(result);
+        return result;
+    }
+}
+
+
+//Second Solution
+
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
