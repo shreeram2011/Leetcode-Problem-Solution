a72dd14e0edd9009183fdc1a719cc6d423a8382e
Shreeram Yadav <113875685+shreeram2011@users.noreply.github.com>
2024-11-17 14:35:14 +0530

    Time: 0 ms (100%), Space: 44.6 MB (16.87%) - LeetHub

---
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int r = mat.length;
+        int c = mat[0].length;
+        int sum = 0;
+
+        for(int rowNo = 0; rowNo < r; rowNo ++){
+            int primaryDiagonal = mat[rowNo][rowNo];
+            int secondaryDiagonal = mat [rowNo][c - 1 - rowNo];
+            sum += primaryDiagonal;
+            if(rowNo != (c - 1 - rowNo))
+                sum += secondaryDiagonal;
+        }
+
+        return sum;
+    }
+}
